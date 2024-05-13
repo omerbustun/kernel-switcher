3314d4caa1d11d1cf91e283337a6f3e1a76525ee
Ömer Üstün <41573243+omerbustun@users.noreply.github.com>
2024-05-13 23:27:21 +0300

    Revert "Migrate from GTK3 to GTK4"
    This reverts commit 191fcc6950805dd839a493248b2229d776ddb3e6.

--- src/Main.py
+++ src/Main.py
@@ -2,8 +2,9 @@ import os
 import gi
 import json
 
-gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk, Adw
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
 
 def load_kernel_versions():
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -19,20 +20,19 @@ def load_kernel_versions():
         print("Error: There was an issue decoding the kernels.json file.")
         return []
 
-class KernelSelector(Gtk.ApplicationWindow):
-    def __init__(self, app):
-        super().__init__(application=app)
-        self.set_title("Kernel Selector")
-        self.set_default_size(400, 100)
+
+class KernelSelector(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Kernel Selector")
         self.set_border_width(10)
 
         self.combo = Gtk.ComboBoxText()
         self.combo.connect("changed", self.on_combo_changed)
         self.load_kernels_into_combo()
 
-        layout = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        layout.append(self.combo)
-        self.set_child(layout)
+        layout = Gtk.Box(spacing=6)
+        layout.pack_start(self.combo, True, True, 0)
+        self.add(layout)
 
     def load_kernels_into_combo(self):
         kernels = load_kernel_versions()
@@ -44,16 +44,9 @@ class KernelSelector(Gtk.ApplicationWindow):
         if text is not None:
             print(f"Selected Kernel: {text}")
 
-class MyApp(Gtk.Application):
-    def __init__(self):
-        super().__init__()
 
-    def do_activate(self):
-        win = KernelSelector(self)
-        win.show()
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-app = MyApp()
-app.run()
+if __name__ == "__main__":
+    win = KernelSelector()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
